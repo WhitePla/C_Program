e80d547d3a3a928ec7419666983581c414828566
WhilePla <geme0323@gmail.com>
2026-04-19 19:45:09 +0900

    EX5 complete

--- EX5.c
+++ EX5.c
@@ -152,19 +152,234 @@ void ex06(){
 
 }
 
-void ex07(){}
+void ex07(){
 
-void ex08(){}
+    char stra[100], strb[100];
 
-void ex09(){}
+    while(1){
 
-void ex10(){}
+        printf("문자열입력 : ");
+    
+        fgets(stra, sizeof(stra), stdin);
 
-void ex11(){}
+        if(stra[0] == '\n') break;
+
+        for(int i = 0; stra[i]; i++){
+        
+            if(stra[i] == '\n'){
+
+                stra[i] = '\0';
+                break;
+
+            }
+        }
+
+        printf("문자열입력 : ");
+
+        fgets(strb, sizeof(strb), stdin);
+
+        if(strb[0] == '\n') break;
+    
+        for(int i = 0; strb[i]; i++){
+        
+            if(strb[i] == '\n'){
+
+                strb[i] = '\0';
+                break;
+
+            }
+        }
+
+        int i = 0;
+
+        while(stra[i] && strb[i]){ // 서로 참인지만 확인
+
+            // 서로 값이 같은지 확인 만약 서로 다르다면 그자리에서 break
+            if(stra[i] != strb[i]) break; //만약 apple appel이라면 [3]이 다르기에 멈춤
+
+            i++;
+
+        }
+
+        printf("%s, %s, ", stra, strb);
+
+        if(stra[i] == strb[i]){
+
+            printf("같음");
+            
+        }else{
+            
+            printf("다름");
+            
+        }
+
+        printf("\n\n");
+
+    }
+
+}
+
+void ex08(){
+
+    char str[2][100];
+    int i, j;
+
+
+    while(1){
+
+        for(i = 0; i < sizeof(str) / sizeof(str[0]); i++){
+            printf("문자열 입력 : ");
+
+            fgets(str[i], sizeof(str[i]), stdin);
+
+            if(str[i][0] == '\n') break;
+
+            for(j = 0; ; j++){
+                if(str[i][j] == '\n'){
+                    
+                    str[i][j] = '\0';
+                    
+                    break;
+                }
+            }
+
+        }
+
+        i = 0;
+
+        while(str[0][i] && str[1][i]){
+
+            if(str[0][i] != str[1][i]) break;
+
+            i++;
+        }
+
+        if(str[0][j] == str[1][i]){
+            printf("같음");
+        }else{
+            printf("다름");
+        }
+
+        printf("\n\n");
+
+    }
+
+}
+
+void ex09(){
+
+    int from[4], sum = 0, max, min;
+
+    for(int i = 0; i < sizeof(from) / sizeof(int); i++){
+
+        printf("정수 입력 : ");
+        scanf("%d", &from[i]);
+
+        sum += from[i];
+
+        min = from[0], max = from[0];
+
+        max = from[i] > max ? from[i] : max;
+        min = from[i] < min ? from[i] : min;
+
+    }
+
+    printf("\n합 = %d\n가장 큰 수  = %d\n가장 작은 수 = %d", sum, max, min);
+
+}
+
+void ex10(){
+
+    int from[4], to[4];
+
+    for(int i = 0; i < sizeof(from) / sizeof(int); i++){
+
+        printf("정수 입력 : ");
+
+        scanf("%d", &from[i]);
+
+        to[i] = from[i];
+
+    }
+
+    for(int i = 0; i < sizeof(from) / sizeof(int); i++){
+
+        printf("from배열: %d, to배열: %d\n", from[i], to[i]);
+
+    }
+
+}
+
+void ex11(){
+
+    int count[5];
+    char num[100];
+
+    for(int i = 0; i < sizeof(count) / sizeof(int); i++){
+        count[i] = 0;
+    }
+
+    printf("0에서 4까지의 정수 입력 : ");
+
+    fgets(num, sizeof(num), stdin);
+
+    if(num[strlen(num) - 1] == '\n') num[strlen(num) - 1] = '\n';
+
+    for(int i = 0; i < sizeof(num) / sizeof(char); i++){
+
+        int index = num[i] - '0';
+
+        if( 0 <= index && index <= 4){
+            
+            count[index]++;
+
+        }
+        
+    }
+
+    int max = count[0] , max_val = 0;
+
+    for(int i = 0; i < 5; i++){
+
+        if( count[i] > max){
+
+            max = count[i];
+            max_val = i;
+
+        }
+
+    }
+
+    printf("가장 많이 입력받은 수 = %d\n", max_val);
+    printf("입력 받은 횟수 = %d\n", max);
+
+}
 
 void ex12(){
 
+    int num[100];
 
+    while(1){
+
+        printf("정수입력 : ");
+        scanf("%d", &num[0]);
+
+        while(getchar() != '\n');
+
+        if(num[0] == 0) break;
+
+        printf("    입력된 %d의 이진수는  : ", num[0]);
+
+        for(int i = num[0]; i > 0; i = i >> 1){
+
+            i & num[0] ? printf("1") : printf("0");
+
+        }
+
+        printf("\n");
+
+    }
+    
 
 }
 
@@ -177,13 +392,13 @@ int main(){
     // ex03();
     // ex04();
     // ex05();
-    ex06();
+    // ex06();
     // ex07();
     // ex08();
     // ex09();
     // ex10();
     // ex11();
-    // ex12();
+    ex12();
 
     return 0;
 
